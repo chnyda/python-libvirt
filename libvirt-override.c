3c1139b62e4a578f32d562f2cdc09f9420c713f8
Pavel Hrdina <phrdina@redhat.com>
2016-04-18 16:53:50 +0200

    fix crash in getAllDomainStats
    Commits 1d39dbaf and 827ed9b4 broke the libvirt-python API by removing
    virDomainRef() and virDomainFree().  virDomainStatsRecordListFree() will
    free that domain pointer and later when virDomain (python object) call
    its destructor and tries to free that same pointer again.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1326839
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- libvirt-override.c
+++ libvirt-override.c
@@ -8200,6 +8200,7 @@ convertDomainStatsRecord(virDomainStatsRecordPtr *records,
     PyObject *py_retval;
     PyObject *py_record;
     PyObject *py_record_stats = NULL;
+    virDomainPtr dom = NULL;
     size_t i;
 
     if (!(py_retval = PyList_New(nrecords)))
@@ -8211,9 +8212,12 @@ convertDomainStatsRecord(virDomainStatsRecordPtr *records,
 
         VIR_PY_LIST_SET_GOTO(py_retval, i, py_record, error);
 
+        dom = records[i]->dom;
+        virDomainRef(dom);
         VIR_PY_TUPLE_SET_GOTO(py_record, 0,
-                              libvirt_virDomainPtrWrap(records[i]->dom),
+                              libvirt_virDomainPtrWrap(dom),
                               error);
+        dom = NULL;
 
         if (!(py_record_stats = getPyVirTypedParameter(records[i]->params,
                                                        records[i]->nparams)))
@@ -8225,6 +8229,8 @@ convertDomainStatsRecord(virDomainStatsRecordPtr *records,
     return py_retval;
 
  error:
+    if (dom)
+        virDomainFree(dom);
     Py_XDECREF(py_retval);
     return NULL;
 }
